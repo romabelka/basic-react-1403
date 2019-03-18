873859289af0c566b440a4b13646a4c4c83f71a3
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-03-18 21:50:22 +0200

    add user input

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ArticleList from './article-list'
 
 function App({ articles }) {
+  const [username, setUsername] = useState('Roma')
+  const handleUserChange = (ev) => {
+    const { value } = ev.target
+    setUsername(value.length < 10 ? value : '')
+  }
+
   return (
     <>
       <h1>Article App</h1>
+      Username:{' '}
+      <input
+        type="text"
+        value={username}
+        onChange={handleUserChange}
+        style={{
+          color: username.length < 5 ? 'red' : 'black'
+        }}
+      />
       <ArticleList articles={articles} />
     </>
   )
