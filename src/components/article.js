7878de835e16e00174f0fa2de4f3b767cb7099f9
niki_mikky <niki.mikkym@gmail.com>
2019-03-19 23:08:42 +0200

    ht1

--- src/components/article.js
+++ src/components/article.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import CommentsList from './Comments/CommentsList'
 
 function Article({ isOpen, article, onBtnClick }) {
   const text = isOpen ? 'close' : 'open'
+  const articleComments = article.comments
+
+  console.log(articleComments, 'COMMENTS TO THIS ARTICLE')
+
   return (
     <div>
       <h3>{article.title}</h3>
       <button onClick={onBtnClick}>{text}</button>
       {getBody({ isOpen, article })}
+      <CommentsList comments={articleComments} />
     </div>
   )
 }
