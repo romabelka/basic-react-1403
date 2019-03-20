52b7d698efa9bb546987992cfc5f4d2979995316
Диана Палий <dpaliy@dpaliy.oggy.co>
2019-03-20 14:56:07 +0300

    Rewrite day picker by hooks

--- src/components/dayPicker/index.js
+++ src/components/dayPicker/index.js
@@ -1,59 +1,23 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import DayPicker, { DateUtils } from 'react-day-picker'
 import 'react-day-picker/lib/style.css'
 import './index.css'
 
-export default class DayPickerInstance extends Component {
-  static defaultProps = {
-    numberOfMonths: 2
-  }
+export default function DayPickerInstance() {
+  const [state, setState] = useState({ from: undefined, to: undefined })
+  const { from, to } = state
+  const range = from && to && `from ${from.toLocaleDateString()} to ${to.toLocaleDateString()}`
+  const handleDayClick = (day) => setState(DateUtils.addDayToRange(day, state))
 
-  constructor(props) {
-    super(props)
-    this.handleDayClick = this.handleDayClick.bind(this)
-    this.handleResetClick = this.handleResetClick.bind(this)
-    this.state = this.getInitialState()
-  }
+  return (
+    <div className="range">
+      <DayPicker
+        className="Selectable"
+        selectedDays={[from, { from, to }]}
+        onDayClick={handleDayClick}
+      />
 
-  getInitialState() {
-    return {
-      from: undefined,
-      to: undefined
-    }
-  }
-  handleDayClick(day) {
-    const range = DateUtils.addDayToRange(day, this.state)
-    this.setState(range)
-  }
-  handleResetClick() {
-    this.setState(this.getInitialState())
-  }
-  render() {
-    const { from, to } = this.state
-    const modifiers = { start: from, end: to }
-    return (
-      <div className="RangeExample">
-        <p>
-          {!from && !to && 'Please select the first day.'}
-          {from && !to && 'Please select the last day.'}
-          {from &&
-            to &&
-            `Selected from ${from.toLocaleDateString()} to
-                ${to.toLocaleDateString()}`}{' '}
-          {from && to && (
-            <button className="link" onClick={this.handleResetClick}>
-              Reset
-            </button>
-          )}
-        </p>
-        <DayPicker
-          className="Selectable"
-          numberOfMonths={this.props.numberOfMonths}
-          selectedDays={[from, { from, to }]}
-          modifiers={modifiers}
-          onDayClick={this.handleDayClick}
-        />
-      </div>
-    )
-  }
+      <p>{range}</p>
+    </div>
+  )
 }
