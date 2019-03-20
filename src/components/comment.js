3da815650f7e921fa98c9743ad6ed4f2a2e19423
Alex Panteleev <alex.p.panteleev@gmail.com>
2019-03-20 20:10:23 +0300

    copmlete toggling comments

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Comment({ userName, text }) {
+  const [isOpen, setState] = useState(false)
+
+  const toggle = () => setState(!isOpen)
+
   return (
     <div>
       <h3>{userName}</h3>
-      <p>{text}</p>
+      <button onClick={toggle}>show comment</button>
+      {isOpen ? <p>{text}</p> : ''}
     </div>
   )
 }
