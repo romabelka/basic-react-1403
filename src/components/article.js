4a0e03d3ff7f3f9a3fb14bb4042f4084b39a3759
Vitalii <am1k>
2019-03-24 19:01:58 +0200

    add test

--- src/components/article.js
+++ src/components/article.js
@@ -6,7 +6,10 @@ function Article(props) {
   const { isOpen, article, onBtnClick } = props
   const text = isOpen ? 'close' : 'open'
   return (
-    <div ref={setContainerRef}>
+    <div
+      ref={setContainerRef}
+      className="test--article"
+    >
       <h3>{article.title}</h3>
       <button onClick={onBtnClick} className="test--article__btn">
         {text}
@@ -36,7 +39,9 @@ Article.propTypes = {
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     text: PropTypes.string
-  })
+  }).isRequired,
+  onBtnClick: PropTypes.func,
+  isOpen: PropTypes.bool
 }
 
 export default Article
