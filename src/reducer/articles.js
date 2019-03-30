c2cc5f8cc8fe742fff8e10ef30538cb92c85545c
vdiachenko <dev.vdiachenko@gmail.com>
2019-03-30 11:33:47 +0300

    Convert articles array to key-value object storage

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,12 +1,22 @@
-import { normalizedArticles as defaultArticles } from '../fixtures'
+import { normalizedArticles } from '../fixtures'
 import { DELETE_ARTICLE } from '../constants'
+
+const defaultArticles = normalizedArticles.reduce(
+  (acc, article) => ({
+    ...acc,
+    [article.id]: article
+  }),
+  {}
+)
 
 export default (articlesState = defaultArticles, action) => {
   const { type, payload } = action
 
   switch (type) {
     case DELETE_ARTICLE:
-      return articlesState.filter((article) => article.id !== payload.id)
+      const articles = { ...articlesState }
+      delete articles[payload.id]
+      return articles
 
     default:
       return articlesState
