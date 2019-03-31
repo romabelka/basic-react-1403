b1872583ccb5057dd8568b2c91fec95f0462341b
Alena Ulasava <lena.vl.94@gmail.com>
2019-03-31 15:47:14 +0300

    HT4.1 удаление статьи

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -13,7 +13,8 @@ export default (articlesState = defaultArticles, action) => {
   const { type, payload } = action
   switch (type) {
     case DELETE_ARTICLE:
-      return articlesState.filter((article) => article.id !== payload.id)
+      delete articlesState[payload.id]
+      return articlesState
 
     default:
       return articlesState
