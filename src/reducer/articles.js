e4916e33ee904b6b28f4d16f67c3c733275b9b44
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-04-01 22:08:11 +0300

    add simple immutable example

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,25 +1,19 @@
 import { DELETE_ARTICLE, ADD_COMMENT } from '../constants'
 import { normalizedArticles as defaultArticles } from '../fixtures'
 import { arrToMap } from './utils'
+import { OrderedMap, fromJS, List } from 'immutable'
 
-export default (articles = arrToMap(defaultArticles), action) => {
+export default (articles = fromJS(arrToMap(defaultArticles)), action) => {
   const { type, payload, randomId } = action
 
   switch (type) {
     case DELETE_ARTICLE:
-      const articlesCopy = { ...articles }
-      delete articlesCopy[payload.id]
-      return articlesCopy
+      return articles.delete(payload.id)
 
     case ADD_COMMENT:
-      const article = articles[payload.articleId]
-      return {
-        ...articles,
-        [payload.articleId]: {
-          ...article,
-          comments: (article.comments || []).concat(randomId)
-        }
-      }
+      return articles.updateIn([payload.articleId, 'comments'], new List([]), (comments) =>
+        comments.push(randomId)
+      )
 
     default:
       return articles
